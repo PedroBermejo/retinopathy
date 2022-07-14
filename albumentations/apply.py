f82f5ccf12156b65541c83b76ec87a3e6dd39ae9
Pedro  Bermejo <pedrobermejo@192.168.1.8>
2022-07-14 00:39:26 -0500

    Clear albu folders, general env yml

--- albumentations/apply.py
+++ albumentations/apply.py
@@ -4,12 +4,25 @@ import random
 import os
 import re
 
-read_path = "/Users/pbermejo/Documents/Master/retinopathy-dataset/test/good/"
-write_good_sample_path = "/Users/pbermejo/Documents/Master/retinopathy-dataset/test/good_sample/"
-write_bad_blur_sample_path = "/Users/pbermejo/Documents/Master/retinopathy-dataset/test/bad_blur_sample/"
-write_bad_gauss_noise_sample_path = "/Users/pbermejo/Documents/Master/retinopathy-dataset/test/bad_gauss_noise_sample/"
-write_bad_random_fog_sample_path = "/Users/pbermejo/Documents/Master/retinopathy-dataset/test/bad_random_fog_sample/"
+read_path = "/Users/pedrobermejo/Documents/Master/retinopathy-dataset/test/good/"
+write_good_sample_path = "/Users/pedrobermejo/Documents/Master/retinopathy-dataset/test/good_sample/"
+write_bad_blur_sample_path = "/Users/pedrobermejo/Documents/Master/retinopathy-dataset/test/bad_blur_sample/"
+write_bad_gauss_noise_sample_path = "/Users/pedrobermejo/Documents/Master/retinopathy-dataset/test/bad_gauss_noise_sample/"
+write_bad_random_fog_sample_path = "/Users/pedrobermejo/Documents/Master/retinopathy-dataset/test/bad_random_fog_sample/"
 
+# First clear folders
+folders_to_clear = [write_good_sample_path,
+                    write_bad_blur_sample_path,
+                    write_bad_gauss_noise_sample_path,
+                    write_bad_random_fog_sample_path]
+
+for folder_path in folders_to_clear:
+    images = [name for name in os.listdir(folder_path)]
+    for image_path in images:
+        os.remove(os.path.join(folder_path, image_path))
+
+
+# List images in good folder and take a sample
 listGoodImages = [
             name for name in os.listdir(read_path)
             if not re.match(r'[\w,\d]+\.[json]{4}', name)
@@ -20,17 +33,18 @@ sampleGoodImages = random.sample(listGoodImages, 100)
 # print(sampleGoodImages)
 
 transform_blur = A.Compose([
-    A.Blur(always_apply=True, p=1, blur_limit=(13, 15))
+    A.Blur(always_apply=True, p=1, blur_limit=(17, 18))
 ])
 
 transform_gauss_noise = A.Compose([
-    A.GaussNoise(always_apply=True, p=1, var_limit=(10.0, 100.0))
+    A.GaussNoise(always_apply=True, p=1, var_limit=(140.0, 150.0))
 ])
 
 transform_random_fog = A.Compose([
     A.RandomFog(always_apply=True, p=1, fog_coef_lower=0.1, fog_coef_upper=0.2, alpha_coef=0.40)
 ])
 
+# Apply albumentations and save
 for imageName in sampleGoodImages:
     image = cv2.imread(os.path.join(read_path, imageName))
     cv2.imwrite(os.path.join(write_good_sample_path, imageName), image)
@@ -42,7 +56,5 @@ for imageName in sampleGoodImages:
     cv2.imwrite(os.path.join(write_bad_blur_sample_path, imageName), transformedBlurImage)
     cv2.imwrite(os.path.join(write_bad_gauss_noise_sample_path, imageName), transformedGaussNoiseImage)
     cv2.imwrite(os.path.join(write_bad_random_fog_sample_path, imageName), transformedRandomFogImage)
-
-
 
 

4e6da6e6b2a350b7c975ef3c85656f080ed85336
PedroBermejo <pbgterri_11@hotmail.com>
2023-03-02 22:59:20 -0600

    combine box plot

--- histogram/boxplot.py
+++ histogram/boxplot.py
@@ -7,22 +7,35 @@ import pandas as pd
 def main():
     df = pd.read_csv(os.path.join(os.getcwd(), args.path_predicts))
 
-    # create a boxplot using matplotlib
-    plt.boxplot([df['inception_good_images_softmax_left'],
-                 df['inception_good_images_softmax_right']])
+    models = ['inception', 'mobilenet', 'resnet', 'vgg']
+    datasets = ['good_images', 'blur', 'gauss_noise', 'random_fog']
+    replace = {'good_images': 'good',
+               'blur': 'blur',
+               'gauss_noise': 'noise',
+               'random_fog': 'fog'}
+    colors = ['red', 'green', 'blue', 'orange']
 
-    # add x-axis labels
-    plt.xticks([1, 2], ['softmax_left', 'softmax_right'])
+    fig, ax = plt.subplots(ncols=4, figsize=(10, 5))
+    i = 0
 
-    # add y-axis label
-    plt.ylabel('Softmax Probability')
+    for model_name in models:
+        template_df = []
+        for dataset_name in datasets:
+            max_df = pd.DataFrame({replace[dataset_name]: df[[f'{model_name}_{dataset_name}_softmax_left',
+                                   f'{model_name}_{dataset_name}_softmax_right']].max(axis=1)})
 
-    # add title
-    plt.title('Inception Softmax Probabilities')
+            if len(template_df) == 0:
+                template_df = max_df
+            else:
+                template_df = max_df.join(template_df, how='inner')
 
-    # display the plot
+        template_df.boxplot(ax=ax[i], column=list(replace.values()))
+        ax[i].set_title(model_name)
+        i = i + 1
+
+    fig.text(0.04, 0.5, '', va='center', rotation='vertical')
+    plt.tight_layout()
     plt.show()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
